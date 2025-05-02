4d92087d9f60ea90e602e08e19427fcf6eaabf0b
fahadnazeer123 <fahadnazeer578@gmail.com>
2025-05-02 23:26:47 +0530

    remove old server.js

--- public/server.js
+++ public/server.js
@@ -1,16 +1,1 @@
-const express = require('express');
-const path = require('path');
-const app = express();
 
-const port = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/chat', (req, res) => {
-  const msg = req.query.msg || '';
-  res.send(`You said: ${msg}`);
-});
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
